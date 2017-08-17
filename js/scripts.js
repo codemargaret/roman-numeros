02d7d246d665d13a63de60b50ba5537c19a5f10b
javier jimenez and Margaret Berry <margaret@>
2017-08-17 13:47:18 -0700

    different methods to this roman numeros project

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,54 @@
 ///BAck end
-  var romanConverter = [
-    ["I",1],
-    ["IV",4],
-    ["V",5],
-    ["IX",9],
-    ["X",10],
-    ["XL",40],
-    ["L",50],
-    ["XC",90],
-    ["C",100],
-    ["CD",400],
-    ["D",500],
-    ["CM",900],
-    ["M",1000]
+  // var romanConverter = [
+  //   ["I",1],
+  //   ["IV",4],
+  //   ["V",5],
+  //   ["IX",9],
+  //   ["X",10],
+  //   ["XL",40],
+  //   ["L",50],
+  //   ["XC",90],
+  //   ["C",100],
+  //   ["CD",400],
+  //   ["D",500],
+  //   ["CM",900],
+  //   ["M",1000]
+  // ];
+  //
+  // var trojanNumbers = [2,3,6,7,8]
+  var romanNumerals = [
+    ['M', 'MM', 'MMM'],
+    ['C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'],
+    ['X', 'XX', 'XXX', 'XL', 'L', 'LX', 'VXX', 'LXXX', 'XC'],
+    ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX']
   ];
 
-  var trojanNumbers = [2,3,6,7,8]
-
 function converter(userInput) {
-  if (parseInt(userInput) === 0) {
-    return "We can't help you. There is no zero in Roman numerals.";
+  var result = '';
+  if (userInput.length === 4) {
+    result += romanNumerals[0] [userInput[0]-1];
+    if (userInput[1] != '0') {
+      result += romanNumerals[1] [userInput[1]-1];
+    }
   }
-  places = userInput.split("").reverse();
-  alert(places);
-  for(var i = 0; i < romanConverter.length; i++) {
-    if (parseInt(userInput) === romanConverter[i][1]) {
-      return romanConverter[i][0];
-    };
-  };
+
+  //   if (parseInt(userInput) === 0) {
+  //   return "We can't help you. There is no zero in Roman numerals.";
+  // }
+  // places = userInput.split("").reverse();
+  // alert(places);
+  // if (userInput === 1) {
+  //   return ones[i][0];
+  //   console.log(userInput);
+  // }
+  // for(var i = 0; i < romanConverter.length; i++) {
+  //   if (parseInt(userInput) === romanConverter[i][1]) {
+  //     return romanConverter[i][0];
+  //   } else if (userInput === 2) {
+  //     return ones[i][0];
+  //   }
+  // };
+alert(result);
 };
 
 ///front end
@@ -38,6 +59,5 @@ $(document).ready(function() {
     var userInput = $("input#userInput").val();
     // alert(userInput);
     var result = converter(userInput);
-    // alert(result);
   });
 });
